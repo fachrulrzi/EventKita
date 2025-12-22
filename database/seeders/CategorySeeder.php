0577f190255f6113a801ac97c7987bcbca601c93
Andi Naufal Dwi Putra <andinaufaldwiputra@gmail.com>
2025-12-22 21:20:39 +0700

    Add default icon URLs to CategorySeeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -19,31 +19,37 @@ class CategorySeeder extends Seeder
                 'name' => 'Musik',
                 'slug' => 'musik',
                 'description' => 'Event konser, festival musik, dan pertunjukan musik lainnya',
+                'icon_path' => 'https://img.icons8.com/fluency/96/music.png',
             ],
             [
                 'name' => 'Seni',
                 'slug' => 'seni',
                 'description' => 'Pameran seni, galeri, dan event seni lainnya',
+                'icon_path' => 'https://img.icons8.com/fluency/96/paint-palette.png',
             ],
             [
                 'name' => 'Kuliner',
                 'slug' => 'kuliner',
                 'description' => 'Festival makanan, food market, dan event kuliner lainnya',
+                'icon_path' => 'https://img.icons8.com/fluency/96/restaurant.png',
             ],
             [
                 'name' => 'Olahraga',
                 'slug' => 'olahraga',
                 'description' => 'Event olahraga, turnamen, dan kompetisi',
+                'icon_path' => 'https://img.icons8.com/fluency/96/sports-mode.png',
             ],
             [
                 'name' => 'Teknologi',
                 'slug' => 'teknologi',
                 'description' => 'Conference, workshop, dan event teknologi',
+                'icon_path' => 'https://img.icons8.com/fluency/96/laptop.png',
             ],
             [
                 'name' => 'Pendidikan',
                 'slug' => 'pendidikan',
                 'description' => 'Seminar, workshop, dan event edukatif',
+                'icon_path' => 'https://img.icons8.com/fluency/96/education.png',
             ],
         ];
 
